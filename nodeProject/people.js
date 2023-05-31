84e95d6348216d18d77a0edff6e9a558a5026a5f
ahadius <92294306+ahadius@users.noreply.github.com>
2023-05-31 11:10:35 +0200

    Delete nodeProject directory

--- nodeProject/people.js
+++
@@ -1,8 +0,0 @@
-const people = ["Ali", "Mohammed", "Hussen"];
-const age = [20,30,40];
-const str ="asc"
-module.exports = {
-  people,
-  age,
-  str
-}
--- nodeProject/test.js
+++
@@ -1,6 +0,0 @@
-
-const {people, age} = require('./people')
-console.log("hello people " + people, age);
-
-console.log("hello")
-console.log("11111")
