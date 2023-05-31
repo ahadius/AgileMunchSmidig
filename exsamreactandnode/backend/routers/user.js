9ea0dc39b921dee2dcb94745c3365848e22b97fc
ahadius <92294306+ahadius@users.noreply.github.com>
2023-05-31 11:10:12 +0200

    Delete exsamreactandnode directory

--- exsamreactandnode/backend/routers/user.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const routerUser= express.Router();
-const {login, signup, getMe} = require('../controllers/userControllers.js')
-const {Protect} = require('../middleware/Authmiddelware.js') 
-routerUser.post('/login',login)
-routerUser.post('/signup', signup)
-routerUser.get('/me',Protect,getMe)
-module.exports = routerUser
