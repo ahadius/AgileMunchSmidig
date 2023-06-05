43efe0a00314cb6b5aa467520235db7c983a3f6d
mohyus15 <94177387+mohyus15@users.noreply.github.com>
2023-06-05 11:02:29 +0200

    created multiplayes component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Startpage from './StartPage/startpage.js';
 import Navbar from './navbar/navbar.js';
 import DrawingArea from './BoardAssets/board.js';
 //import Background from './background/background';
+import Multi from '../src/multi/multi.js';
 
 function App() {
 	return (
@@ -17,6 +18,7 @@ function App() {
 					path="/board"
 					element={<DrawingArea />}
 				/>
+				<Route exact path="/multi" element={<Multi />} />
 			</Routes>
 		</div>
 	);
--- frontend/src/navbar/navbar.js
+++ frontend/src/navbar/navbar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import DrawingArea from '../BoardAssets/board.js';
 import { Link } from 'react-router-dom';
 import '../App.css';
 
@@ -12,6 +11,7 @@ const Navbar = () => {
 						MUNCH
 						<Link to="./board"></Link>
 					</Link>
+					<Link to="./multi">multiplayers</Link>
 				</div>
 
 				<div className="whiteBox"></div>
