1a977adcb78702f8226db451a54a81f4baab4d70
ahmed edelbijev <ahmadedel0@hotmail.com>
2023-06-05 12:02:34 +0200

    h
    h

--- frontend/src/navbar/navbar.js
+++ frontend/src/navbar/navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
 		<div>
 			<div className="App">
 				<div className="nav-container">
-					<Link to="/" className="Munch">
+				<Link to="./" className="Munch">
 						MUNCH
 						<Link to="./board"></Link>
 					</Link>
