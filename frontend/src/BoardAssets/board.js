132061652b8be036491f229faa8eb555daddc745
bistonKarim <101369895+bistonKarim@users.noreply.github.com>
2023-06-06 13:13:05 +0200

    Ny versjon av drawing bord

--- frontend/src/BoardAssets/board.js
+++ frontend/src/BoardAssets/board.js
@@ -1,187 +1,196 @@
-import React, {useRef, useEffect, useState} from 'react';
-import "./board.css";
 
 
-const DrawingArea = () => {
+
+import React, { useRef, useEffect, useState } from 'react';
+
+const DrawingBoard = ({ username }) => {
     const canvasRef = useRef(null);
-    const linesRef = useRef([]);
-    const isDrawingRef = useRef(false);
-    const [selectedColor, setSelectedColor] = useState('#000000'); // default color is black
-    const [tool, setTool] = useState('pencil'); // default tool is pencil
-    const selectedColorRef = useRef(selectedColor);
-    const toolRef = useRef(tool);
-    const eraserColor = '#FFFFFF'; // The color for the eraser, assuming the canvas background is white
-    const [previousColor, setPreviousColor] = useState(''); // store previous colour when switching to eraser
+    const [drawing, setDrawing] = useState(false);
+    const [color, setColor] = useState('#000000');
+    const [size, setSize] = useState('2');
+    const [history, setHistory] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(-1);
+    const [tool, setTool] = useState('pencil');
+    const [lineStart, setLineStart] = useState(null);
+    const [brushType, setBrushType] = useState('flat');
+    const colorPalette = [
+        '#000000', '#FFFFFF', '#808080', '#B22222', '#A52A2A', '#006400',
+        '#008000', '#008080', '#000080', '#4B0082', '#800080', '#FF0000',
+        '#FF4500', '#FF8C00', '#FFD700', '#ADFF2F', '#7FFF00', '#32CD32',
+        '#00FF00', '#00FA9A', '#00FFFF', '#0000FF', '#8A2BE2', '#9370DB',
+        '#FF00FF', '#1E90FF', '#ADD8E6', '#87CEEB', '#FFB6C1', '#FF69B4',
+        '#FF1493',
+    ];
+    //const [filling, setFilling] = useState(false);
 
 
-    const handleColorChange = (color) => {
-        setSelectedColor(color);
-        selectedColorRef.current = color;
-    };
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
+
+        const draw = (e) => {
+            if (!drawing) return;
+            const rect = canvas.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+            context.lineWidth = size;
+            context.strokeStyle = color;
+    
+            if (brushType === 'round') {
+                context.lineJoin = 'round';
+                context.lineCap = 'round';
+            } else if (brushType === 'flat') {
+                context.lineJoin = 'bevel';
+                context.lineCap = 'square';
+            } else if (brushType === 'textured') {
+                context.lineJoin = 'round';
+                context.lineCap = 'butt';
+                context.lineWidth = size * (Math.round(Math.random()) ? 1 : 0.5);
+            }
+    
+            if (tool === 'pencil') {
+                context.lineTo(x, y);
+                context.stroke();
+                context.beginPath();
+                context.moveTo(x, y);
+            }
+            else if (tool === 'line' && lineStart) {
+                context.putImageData(lineStart.imageData, 0, 0);
+                context.beginPath();
+                context.moveTo(lineStart.x, lineStart.y);
+                context.lineTo(x, y);
+                context.stroke();
+            }
+        }
+    
+
+        const startDrawing = (e) => {
+            const rect = canvas.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+
+            if (tool === 'line') {
+                setLineStart({
+                    x: x,
+                    y: y,
+                    imageData: context.getImageData(0, 0, canvas.width, canvas.height)
+                });
+            }
+
+            setDrawing(true);
+            draw(e);
+        }
+
+        const endDrawing = () => {
+            setDrawing(false);
+            context.beginPath();
+            setLineStart(null);
+            saveToHistory();
+        }
+
+          
+
+
+        canvas.addEventListener('mousedown', startDrawing);
+        canvas.addEventListener('mouseup', endDrawing);
+        canvas.addEventListener('mousemove', draw);
+        //canvas.addEventListener('click', fillArea);
+
+        return () => {
+            canvas.removeEventListener('mousedown', startDrawing);
+            canvas.removeEventListener('mouseup', endDrawing);
+            canvas.removeEventListener('mousemove', draw);
+            //canvas.removeEventListener('click', fillArea);
+
+        };
+    }, [drawing, color, size, tool, brushType]);
+
+    const saveToHistory = () => {
+        const canvas = canvasRef.current;
+        const historyCopy = [...history];
+        historyCopy.push(canvas.toDataURL());
+        setHistory(historyCopy);
+        setCurrentIndex(historyCopy.length - 1);
+    }
 
     const undo = () => {
-        linesRef.current.pop();
-        redraw();
-    };
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
+        if (currentIndex > 0) {
+            setCurrentIndex(currentIndex - 1);
+            const image = new Image();
+            image.src = history[currentIndex - 1];
+            image.onload = () => {
+                context.clearRect(0, 0, canvas.width, canvas.height);
+                context.drawImage(image, 0, 0, canvas.width, canvas.height);
+            };
+        }
+    }
 
     const clearCanvas = () => {
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d');
         context.clearRect(0, 0, canvas.width, canvas.height);
-        linesRef.current = [];
-    };
+        setHistory([]);
+        setCurrentIndex(-1);
+    }
 
-    const redraw = () => {
+
+    /*const fillCanvas = () => {
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d');
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        linesRef.current.forEach((line) => {
-            context.lineJoin = 'round';
-            context.lineCap = 'round';
-            context.lineWidth = 5;
-            context.strokeStyle = line[0].color;
-            line.forEach((point, index) => {
-                if (index === 0) {
-                    context.beginPath();
-                    context.moveTo(point.x, point.y);
-                } else {
-                    context.lineTo(point.x, point.y);
-                    context.stroke();
-                }
-            });
-        });
-    };
-
-
-    // Update the tool in the state instead of directly modifying it
-    const handleToolChange = (newTool) => {
-        if (toolRef.current === 'eraser') {
-            setSelectedColor(previousColor);
-            selectedColorRef.current = previousColor;
-        }
-        toolRef.current = newTool;
-        setTool(newTool);
-        // If the tool is an eraser, set the color to the eraser color
-        if (newTool === 'eraser') {
-            setPreviousColor(selectedColorRef.current);
-            setSelectedColor(eraserColor);
-            selectedColorRef.current = eraserColor;
-        }
-    };
-    useEffect(() => {
-        const canvas = canvasRef.current;
-        const context = canvas.getContext('2d');
-        // Set the actual pixel size based on the desired width and height
-        const width = canvas.clientWidth;
-        const height = canvas.clientHeight;
-        canvas.width = width;
-        canvas.height = height;
-        let currentLine = [];
-
-        // Update the tool in the state when it changes
-        const startDrawing = (e) => {
-            const rect = canvas.getBoundingClientRect();
-            const x = e.clientX - rect.left;
-            const y = e.clientY - rect.top;
-            if (toolRef.current === 'bucket') {
-            } else {
-                isDrawingRef.current = true;
-                currentLine = [{x, y}];
-            }
-        };
-
-
-        const draw = (e) => {
-            if (!isDrawingRef.current) return;
-            const rect = canvas.getBoundingClientRect();
-            const x = e.clientX - rect.left;
-            const y = e.clientY - rect.top;
-            currentLine.push({x, y});
-            drawLine();
-        };
-
-        const stopDrawing = () => {
-            if (!isDrawingRef.current) return;
-            isDrawingRef.current = false;
-            saveLine();
-        };
-
-        const saveLine = () => {
-            const lineWithColor = [...currentLine].map((point) => ({
-                x: point.x,
-                y: point.y,
-                color: selectedColorRef.current,
-            }));
-            linesRef.current.push(lineWithColor);
-            currentLine = [];
-        };
-
-        const drawLine = () => {
-            context.clearRect(0, 0, canvas.width, canvas.height);
-            redraw();
-            context.strokeStyle = selectedColorRef.current;
-            context.lineJoin = 'round';
-            context.lineCap = 'round';
-            context.lineWidth = 5;
-            for (let i = 0; i < currentLine.length - 1; i++) {
-                context.beginPath();
-                context.moveTo(currentLine[i].x, currentLine[i].y);
-                context.lineTo(currentLine[i + 1].x, currentLine[i + 1].y);
-                context.stroke();
-            }
-        };
-
-        const handleResize = () => {
-            const {width, height} = canvas.getBoundingClientRect();
-            canvas.width = width;
-            canvas.height = height;
-            redraw();
-        };
-
-        window.addEventListener('resize', handleResize);
-        canvas.addEventListener('mousedown', startDrawing);
-        canvas.addEventListener('mousemove', draw);
-        canvas.addEventListener('mouseup', stopDrawing);
-        canvas.addEventListener('mouseleave', stopDrawing);
-
-        return () => {
-            // Clean up event listeners
-            window.removeEventListener('resize', handleResize);
-            canvas.removeEventListener('mousedown', startDrawing);
-            canvas.removeEventListener('mousemove', draw);
-            canvas.removeEventListener('mouseup', stopDrawing);
-            canvas.removeEventListener('mouseleave', stopDrawing);
-        };
-    }, []);
-
+        context.fillStyle = color;
+        context.fillRect(0, 0, canvas.width, canvas.height);
+        saveToHistory();
+      };*/
+  
     return (
-        <div
-            className={`canvas-container ${tool === 'pencil' ? 'pencil-cursor' : tool === 'bucket' ? 'bucket-cursor' : 'eraser-cursor'}`}>
-            <canvas
-                ref={canvasRef}
-                className="my-canvas"
-            />
-            <div className="canvas-buttons">
-                <input
-                    type="color"
-                    value={selectedColor}
-                    onChange={(e) => handleColorChange(e.target.value)}
-                />
-                <button onClick={undo}>Undo</button>
-                <button onClick={clearCanvas}>Clear Canvas</button>
-                <button onClick={() => handleToolChange('pencil')}>Pencil</button>
-                <button onClick={() => handleToolChange('bucket')}>Bucket</button>
-                <button onClick={() => handleToolChange('eraser')}>Eraser</button>
+        <div style={{ display: 'grid', gridTemplateColumns: '1fr 800px 1fr', gridTemplateRows: 'auto 600px', gap: '2rem', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <div style={{ gridColumn: '1 / span 3', display: 'flex', justifyContent: 'center', gap: '2rem' }}>
+                <label>
+                    Fargepalett:
+                    {colorPalette.map((color, i) => (
+                        <button
+                            key={i}
+                            style={{ backgroundColor: color, width: '20px', height: '20px' }}
+                            onClick={() => setColor(color)}
+                        />
+                    ))}
+                </label>
+
+                <label>
+                Børstetype:
+                <select value={brushType} onChange={(e) => setBrushType(e.target.value)}>
+                    <option value="flat">Flat</option>
+                    <option value="round">Round</option>
+                    <option value="textured">Textured</option>
+                </select>
+            </label>  
+
+                <label>
+                    Penselfarge:
+                    <input type="color" value={color} onChange={(e) => setColor(e.target.value)} />
+                </label>
             </div>
-        </div>
+            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', gap: '1rem' }}>
+                <label>
+                    Penselstørrelse:
+                    <input type="number" min="1" max="50" value={size} onChange={(e) => setSize(e.target.value)} />
+                </label>
+                <label>
+                    Tegneverktøy:
+                    <select value={tool} onChange={(e) => setTool(e.target.value)}>
+                        <option value="pencil">Pensel</option>
+                        <option value="line">Rett linje</option>
+                    </select>
+                </label>
+                <button onClick={undo}>Angre</button>
+                <button onClick={clearCanvas}>Rens Canvas</button>
 
+            </div>
+            <canvas ref={canvasRef} width="800" height="600" style={{ border: '1px solid black', gridColumn: '2' }} />
+            </div>
     );
-};
+}
 
-
-export default DrawingArea
-
-
-
-
-
+export default DrawingBoard;
