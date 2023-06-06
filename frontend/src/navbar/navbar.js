ce087b97af22bdeac24551e692cf2f54d5731aa8
mohyus15 <94177387+mohyus15@users.noreply.github.com>
2023-06-06 10:14:36 +0200

    install react-bootstarp

--- frontend/src/navbar/navbar.js
+++ frontend/src/navbar/navbar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
+import { Card } from 'react-bootstrap';
 
 const Navbar = () => {
 	return (
 		<div>
+			<Card>hello</Card>
 			<div className="App">
 				<div className="nav-container">
-				<Link to="./" className="Munch">
+					<Link to="./" className="Munch">
 						MUNCH
 						<Link to="./board"></Link>
 					</Link>
