e45a7d82813c474c4ee66c5f1c3fc6eaff581468
mohyus15 <94177387+mohyus15@users.noreply.github.com>
2023-06-07 00:03:58 +0200

    added som user register in server site and added hashed password

---
+++ backend/controllers/RatingController.js
@@ -0,0 +1,8 @@
+const Rating = require('../models/RatingModel.js');
+
+const givRating = () => {
+	console.log('rating fuction is working in controllers');
+};
+module.exports = {
+	givRating,
+};
--- frontend/src/navbar/navbar.js
+++ frontend/src/navbar/navbar.js
@@ -29,10 +29,10 @@ export default Navbar;*/
 import React from 'react';
 //import { Link } from 'react-router-dom';
 import '../App.css';
-import { Navbar, Nav,NavDropdown,} from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-		/*<div>
+/*<div>
 			<Card>hello</Card>
 			<div className="App">
 				<div className="nav-container">
@@ -51,16 +51,57 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
 	return (
-		<Navbar style={{height: '100px'}} collapseOnSelect expand="sm" bg="dark" variant="dark" className="mb-3">
-            <Navbar.Brand style={{fontFamily: "impact", color:"red", fontSize:50,}} as={Link} to="/" className="mx-3">MUNCH</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="ms-auto">
-                    <Nav.Link as={Link} to="/board" style={{fontFamily:"impact", fontSize:"25px"}}>Plays</Nav.Link>
-                    <Nav.Link as={Link} to="/login" style={{fontFamily:"impact", fontSize:"25px"}}>Login</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+		<Navbar
+			style={{ height: '100px' }}
+			collapseOnSelect
+			expand="sm"
+			bg="dark"
+			variant="dark"
+			className="mb-3">
+			<Navbar.Brand
+				style={{
+					fontFamily: 'impact',
+					color: 'red',
+					fontSize: 50,
+				}}
+				as={Link}
+				to="/"
+				className="mx-3">
+				MUNCH
+			</Navbar.Brand>
+			<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+			<Navbar.Collapse id="responsive-navbar-nav">
+				<Nav className="ms-auto">
+					<Nav.Link
+						as={Link}
+						to="/board"
+						style={{
+							fontFamily: 'impact',
+							fontSize: '25px',
+						}}>
+						Plays
+					</Nav.Link>
+					<Nav.Link
+						as={Link}
+						to="/login"
+						style={{
+							fontFamily: 'impact',
+							fontSize: '25px',
+						}}>
+						Login
+					</Nav.Link>
+					<Nav.Link
+						as={Link}
+						to="/result"
+						style={{
+							fontFamily: 'impact',
+							fontSize: '25px',
+						}}>
+						Yourdrawpictures
+					</Nav.Link>
+				</Nav>
+			</Navbar.Collapse>
+		</Navbar>
 	);
 };
 //test dette funker kanskje
